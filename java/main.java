dac042e3552ccb18ff5b1fa94d37d6fe7fff066e
johnnyabouhaidar <johnnyabuhaydar@gmail.com>
2024-09-01 18:32:05 +0300

    added from batch file

--- java/main.java
+++ java/main.java
@@ -1,9 +1,17 @@
 class HelloWorld{
     public static void main(String[] args){
         int num_arr[]={4,2,3,5,6};
-        int sorted_arr[]={};
+        
         for (int i=0;i<num_arr.length;i++){
-            
+            for (int j=0;j<num_arr.length-i-1;j++){
+                if (num_arr[j]>num_arr[j+1]){
+                    num_arr[j]=num_arr[j+1];
+                    num_arr[j+1]=num_arr[j];
+                }
+            }
+        }
+        for (int k=0;k<num_arr.length;k++){
+            System.out.println(num_arr[k]);
         }
     }
 }
