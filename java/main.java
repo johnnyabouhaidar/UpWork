4461b25c8ffbd06d831fb92d763fbf19cea0abc3
johnnyabouhaidar <johnnyabuhaydar@gmail.com>
2024-09-02 17:44:56 +0300

    added from batch file

--- java/main.java
+++ java/main.java
@@ -1,17 +1,13 @@
 class HelloWorld{
     public static void main(String[] args){
-        int num_arr[]={4,2,3,5,6};
-        
-        for (int i=0;i<num_arr.length;i++){
-            for (int j=0;j<num_arr.length-i-1;j++){
-                if (num_arr[j]>num_arr[j+1]){
-                    num_arr[j]=num_arr[j+1];
-                    num_arr[j+1]=num_arr[j];
-                }
-            }
+        try{
+        int sentence = 1;
+        System.out.println(5/0);
+        }catch(Exception ex){
+        System.out.println("ow");    
+        }finally{
+            System.out.println("ow1");    
         }
-        for (int k=0;k<num_arr.length;k++){
-            System.out.println(num_arr[k]);
-        }
+
     }
 }
