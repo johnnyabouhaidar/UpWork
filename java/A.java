bf4081b0fb61b1c52eb770cb4207c26a43bf0071
johnnyabouhaidar <johnnyabuhaydar@gmail.com>
2024-09-05 17:23:49 +0300

    added from batch file

--- java/A.java
+++ java/A.java
@@ -1,17 +1,8 @@
-public class A{ 
-
-    public void method2(float a,float b){ 
-
-
-
-    } 
-
-    public static void main(String args[]){ 
-
-        A a = new A(); 
-
-        a.method2(0,1); 
-
-    } 
-
-} 
+public class A{
+    public static void main(String[] args){
+        int[] numbers ={1,3,4,5};
+        for (int i=0;i<numbers.length;i++){
+            System.out.println(numbers[i]);
+        }
+    }
+}
